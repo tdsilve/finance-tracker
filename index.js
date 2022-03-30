28cd04b91b90bf9fad115fe13f37c2891a07af07
Thais Oliveira <thaidsilve@gmail.com>
2022-03-30 15:23:42 -0300

    add backend files

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const path = require('path');
+
+//Set up express app
+const app = express();
+
+//Connect to mongodb
+mongoose.connect('mongodb://localhost/transactions');
+mongoose.Promise = global.Promise;
+
+//Connect to front end
+app.use(express.static('public'));
+app.use(bodyParser.json())
+
+/*Set '/api'as the first parameter and import routes from api.js*/
+app.use('/api', require('./router/api'));
+
+//Middleware for error handling
+app.use(function(err, req, res, next){
+  console.log(err);
+  /*422 - A requisição está bem formada mas inabilitada para ser seguida devido a erros semânticos. */
+  res.status(422).send({error: err.message});
+})
+
+/*Listen for requests at the port 5001*/
+app.listen(5001, function(){
+    console.log('Listening for requests...');
+  });
+
+app.get('/', function(req, res){
+  console.log('Ready!');
+  res.send({name: 'Ready!'});
+})
+
+
---
+++ router/api.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const Transactions = require("../modules/transactions");
+const router = express.Router();
+
+//get a list of transactions
+router.get("/transactions", function (req, res, next) {
+  // res.send({ type: "GET" });
+  Transactions.find({}).then(function(transactions){
+    res.send(transactions);
+  })
+});
+
+//get a new transaction
+router.post("/transactions", function (req, res, next) {
+  // console.log(req.body);
+  Transactions.create(req.body).then(function(transaction){
+    res.send(transaction);
+  }).catch(next);
+  // res.send({ type: "POST", name: req.body.name, rank: req.body.rank })
+});
+
+//update database
+router.put("/transactions/:id", function (req, res, next) {
+  Transactions.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(transaction){
+    //Find the updated information
+    Transactions.findOne({_id: req.params.id}).then(function(transaction){
+       //Send the updated information
+        res.send(transaction);
+    })
+   
+  });
+  // res.send({ type: "PUT" });
+});
+
+//delete a transaction from database
+router.delete("/transactions/:id", function (req, res, next) {
+  // console.log(req.params.id)
+  Transactions.findByIdAndRemove({_id: req.params.id}).then(function(transaction){
+    res.send(transaction);
+  });
+  // res.send({ type: "DELETE" });
+});
+
+//Export routers
+module.exports = router;
