3732c5dacaee93d38c1bb525a3bf04bb42a6de2e
Thais Oliveira <thaidsilve@gmail.com>
2022-04-01 17:53:13 -0300

    finish filter in react

--- src/components/Data.js
+++ src/components/Data.js
@@ -6,6 +6,7 @@ export default function Data() {
   const [data, setData] = useState([]);
   const [queryDescription, setQueryDescription] = useState('');
   const [queryValue, setQueryValue] = useState('');
+  const [queryCategory, setQueryCategory] = useState('');
 
   // Get data from database
   useEffect(function (){
@@ -44,7 +45,18 @@ export default function Data() {
           <input type="text" value={queryDescription} onChange={(e) => setQueryDescription(e.target.value)}  className='form-control mb-2'/>
           <label className='mb-2'>Value:</label>
           <input type="number" value={queryValue} onChange={(e) => setQueryValue(+e.target.value)}  className='form-control'/>
-          <button>Search</button>
+          <label className='m-2'>Category:</label>
+          <select className='form-select' value={queryCategory} onChange={(e) => setQueryCategory(e.target.value)}>
+            <option value="salary">Salary</option>
+            <option value="food">Food</option>
+            <option value="transport">Transport</option>
+            <option value="house">House</option>
+            <option value="other">Other</option>
+          </select>
+          <div className='text-center'>
+            <button className="btn btn-primary m-2 text-center">Search</button>
+          </div>
+          
         </form>
        
 
