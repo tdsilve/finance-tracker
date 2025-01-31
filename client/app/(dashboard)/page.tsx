bb3ee5158eb836f7725eb0144a2e94040f59bd48
thais <thais>
2025-01-30 22:34:54 -0300

    create client

---
+++ client/app/(dashboard)/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import { Button } from "~/components/ui/button";
+
+
+export default function Home() {
+  return (
+   <div className="flex items-center w-full justify-center h-full">
+    <Button >hey</Button>
+   </div>
+  );
+}
