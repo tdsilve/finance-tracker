169cb7a7a2e6f1fe01290b23da657afeffd2909f
thais <thais>
2025-02-06 12:02:59 -0300

    set up protect routes in middleware

--- client/middleware.ts
+++ client/middleware.ts
@@ -3,24 +3,25 @@ import { NextResponse, NextRequest } from "next/server";
 const publicRoutes = ["/sign-in", "/sign-up"];
 
 export function middleware(req: NextRequest) {
-    const token = req.cookies.get("user")?.value;
-    const isAuthenticated = !!token;
-    const { pathname, origin } = req.nextUrl;
-    const isPublicRoute = publicRoutes.find((route) => pathname.startsWith(route));
+  const token = req.cookies.get("user")?.value;
+  const isAuthenticated = !!token;
+  const { pathname, origin } = req.nextUrl;
+  const isPublicRoute = publicRoutes.find((route) =>
+    pathname.startsWith(route),
+  );
 
-    if (!isAuthenticated && !isPublicRoute) {
-        return NextResponse.redirect(new URL("/sign-in", origin));
-    } 
-  
-    return NextResponse.next();
+  if (!isAuthenticated && !isPublicRoute) {
+    return NextResponse.redirect(new URL("/sign-in", origin));
+  }
 
+  return NextResponse.next();
 }
 
- export const config = {
-    matcher: [
-      // Skip Next.js internals and all static files, unless found in search params
-       '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-      // Always run for API routes
-      '/(api|trpc)(.*)'
-    ],
-   }
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
