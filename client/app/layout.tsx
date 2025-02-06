15671f6182a7013266a637fed2c173ed62109710
thais <thais>
2025-02-06 14:59:20 -0300

    Set up query client in root layout

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { QueryProvider } from "~/context/QueryClient";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <QueryProvider>
         {children}
+        </QueryProvider>
       </body>
     </html>
   );
