84bfbc0edec2e7dace92a235dbbd5d34a4bbbdb7
thais <thais>
2025-02-06 18:44:07 -0300

    update public route layout

--- client/app/(public)/layout.tsx
+++ client/app/(public)/layout.tsx
@@ -1,11 +1,13 @@
+import { Grid } from "~/components/generic/Grid";
+
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2 grid-flow-col place-items-center">
-      {children}
-    </div>
+    <Grid className="min-h-screen w-full lg:grid-cols-2" cols={1} col placeItems="center">
+       {children}
+    </Grid>
   );
 }
