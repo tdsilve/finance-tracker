19c6aabd16e2eadd18bd64d98a8616fe934052cd
thais <thais>
2025-02-08 00:43:18 -0300

    add link to sign in page

--- client/components/auth/reset-password/ResetPassword.tsx
+++ client/components/auth/reset-password/ResetPassword.tsx
@@ -5,6 +5,7 @@ import { Flex } from "~/components/generic/Flex";
 import { Header } from "../Header";
 import { ErrorAlert } from "~/components/generic/alert/ErrorAlert";
 import Link from "next/link";
+import { PrimaryLink } from "~/components/generic/link/PrimaryLink";
 
 type ResetPasswordProps = { token: string };
 
@@ -31,6 +32,14 @@ export const ResetPassword = ({ token }: ResetPasswordProps) => {
         subtitle="Enter your new password below to securely regain access to your account."
       />
       <ResetPasswordForm token={token} />
+      <div className="text-sm text-center">
+      Go back to
+      <PrimaryLink href="/sign-in" className="mx-1">
+         sign in 
+      </PrimaryLink>
+      page
+      </div>
+     
     </Flex>
   );
 };
