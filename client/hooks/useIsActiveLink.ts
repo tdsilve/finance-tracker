18e13b8cb4574d191a4b60316db9d6c87ae7b68e
thais <thais>
2025-02-12 23:04:10 -0300

    Fix: returns true for "/"

--- client/hooks/useIsActiveLink.ts
+++ client/hooks/useIsActiveLink.ts
@@ -3,5 +3,6 @@ import { usePathname } from "next/navigation";
 
 export const useIsActiveLink = (href: string) => {
   const pathname = usePathname();
+  if (href === "/") return pathname === "/";
   return pathname.startsWith(href);
 };
